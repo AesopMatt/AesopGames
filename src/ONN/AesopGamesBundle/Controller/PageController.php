d948fe24087f9af91e918269fca2bd0354d2c2de
Margaret Staples <margaret@olympicnorthwest.com>
2014-01-11 06:19:26 -0800

    initial layout and page setupsetup

--- src/ONN/AesopGamesBundle/Controller/PageController.php
+++ src/ONN/AesopGamesBundle/Controller/PageController.php
@@ -9,6 +9,43 @@ class PageController extends Controller
 {
     public function indexAction(Request $request)
     {
+        $session = $request->getSession();
+        $session->set('page','index');
         return $this->render('ONNAesopGamesBundle:Page:index.html.twig');
     }
+
+    public function aboutAction(Request $request)
+    {
+        $session = $request->getSession();
+        $session->set('page','about');
+        return $this->render('ONNAesopGamesBundle:Page:about.html.twig');
+    }
+
+    public function gamesAction(Request $request)
+    {
+        $session = $request->getSession();
+        $session->set('page','games');
+        return $this->render('ONNAesopGamesBundle:Page:games.html.twig');
+    }
+
+    public function blogAction(Request $request)
+    {
+        $session = $request->getSession();
+        $session->set('page','blog');
+        return $this->render('ONNAesopGamesBundle:Page:blog.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+        $session = $request->getSession();
+        $session->set('page','contact');
+        return $this->render('ONNAesopGamesBundle:Page:contact.html.twig');
+    }
+
+    public function navbarAction(Request $request)
+    {
+        return $this->render('ONNAesopGamesBundle:Navbar:navbar.html.twig');
+    }
+
+
 }
